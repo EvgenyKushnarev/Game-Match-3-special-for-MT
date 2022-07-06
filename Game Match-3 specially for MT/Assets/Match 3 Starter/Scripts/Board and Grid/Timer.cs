14e034b2ff50a0040bfc56c750c5dd9a420a4b29
KushnarevEV <94463547+EvgenyKushnarev@users.noreply.github.com>
2022-07-06 04:39:22 +0300

    Update Game Match-3 specially for MT.zip

---
+++ Game Match-3 specially for MT/Assets/Match 3 Starter/Scripts/Board and Grid/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+  
+{
+    public float timeStart = 60;
+    public float timeEnd = 0;
+    public Text timerText;
+
+    void Start()
+    {
+        timerText.text = timeStart.ToString();
+        
+    }
+
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        timerText.text = Mathf.Round(timeStart).ToString();
+
+        if (timeStart <= 0)
+        {
+            timeStart = 0;
+        }
+    }
+}
